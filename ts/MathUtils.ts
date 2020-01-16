c4223b28c92f7ec3179dadc425b68f2b364badb5
0-micon <0.miconst@gmail.com>
2020-01-16 10:53:28 +0300

    + byte conversion

--- ts/MathUtils.ts
+++ ts/MathUtils.ts
@@ -8,4 +8,32 @@ namespace MathUtils {
   export function randomIneger(minValue: number, maxValue: number): number {
     return Math.floor(Math.random() * (maxValue - minValue)) + minValue;
   }
+
+  const CODE_0 = '0'.charCodeAt(0);
+  const CODE_A = 'a'.charCodeAt(0);
+
+  export function byteToCode(n: number): number {
+    if (n >= 0 && n < 10) {
+      return CODE_0 + n;
+    } else {
+      return CODE_A + n - 10;
+    }
+  }
+
+  export function codeToByte(code: number): number {
+    if (code >= CODE_A) {
+      return 10 + code - CODE_A;
+    } else {
+      return code - CODE_0;
+    }
+  }
+
+  export function byteArrayToString(arr: number[]): string {
+    const codes = arr.map(n => byteToCode(n));
+    return String.fromCharCode(...codes);
+  }
+
+  export function byteAt(str: string, index: number): number {
+    return codeToByte(str.charCodeAt(index));
+  } 
 }
